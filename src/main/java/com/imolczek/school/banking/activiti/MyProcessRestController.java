bee7bb3789e14a7fa1c5624329c783160556be7b
Fabian Bouché <fabian.bouche@gmail.com>
2019-02-03 14:20:20 +0100

    Updated API

--- src/main/java/com/imolczek/school/banking/activiti/MyProcessRestController.java
+++ src/main/java/com/imolczek/school/banking/activiti/MyProcessRestController.java
@@ -1,5 +1,6 @@
 package com.imolczek.school.banking.activiti;
 
+import java.util.List;
 import java.util.Map;
 
 import org.activiti.engine.FormService;
@@ -8,6 +9,7 @@ import org.activiti.engine.TaskService;
 import org.activiti.engine.runtime.ProcessInstance;
 import org.activiti.engine.task.Task;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -104,5 +106,18 @@ public class MyProcessRestController {
 	    
 		return "Denied credit request";
 	}
+	
+	@GetMapping("/list")
+	public String getProcessInstances() {
+		List<ProcessInstance> instancesList = runtimeService.createProcessInstanceQuery().list();
+		return new Integer(instancesList.size()).toString();
+	}
 
+	@GetMapping("/consult/{processInstanceId}")
+	public String getProcessInstanceDetails(@PathVariable String processInstanceId) {
+		ProcessInstance instance = runtimeService.createProcessInstanceQuery().processInstanceId(processInstanceId).singleResult();
+		
+		return (String) runtimeService.getVariable(processInstanceId, "email");
+	}
+	
 }
